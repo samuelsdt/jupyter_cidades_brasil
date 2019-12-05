59364c741e4c164a5b57a9f9acc286cdf019ba68
samuelsdt <samuelsdt@hotmail.com>
2019-12-04 23:20:31 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
 
    
-    install_requires=['ipyleaflet', 'ipywidgets', 'branca'],  # Optional
+    install_requires=['ipyleaflet', 'ipywidgets', 'branca'], 
 
 
     
