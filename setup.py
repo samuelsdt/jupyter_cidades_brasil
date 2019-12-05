f961f8258a165db8b77f12337358417c7b104e92
samuelsdt <samuelsdt@hotmail.com>
2019-12-05 15:03:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
    
     install_requires=['ipyleaflet', 'ipywidgets', 'branca'], 
     
-    data_files=[(['brlcities/data.geo.json'])],
+    data_files=[('brlcities/data.geo.json')],
     include_package_data=True
    
 )
