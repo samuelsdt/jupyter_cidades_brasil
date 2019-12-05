03f4bdd7161cb91ec5def4ed8647da67b9856391
samuelsdt <samuelsdt@hotmail.com>
2019-12-05 01:39:02 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
 
     packages=['brlcities'],
     
-    py_modules=["my_module"],
+    py_modules=["brlcities"],
   
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
 
